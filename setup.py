055418bf1810a03020710344cff4640c549e3348
unknown <Scopatz@.(none)>
2010-09-11 13:22:40 -0500

    New Windows distribution.

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from distutils.core import setup
 from distutils.extension import Extension
 
-setup(name="BriPy",
-	version='0.21',
+setup(name="MetaSci",
+	version='0.23',
 	description='MetaSci',
 	author='Anthony Scopatz',
 	author_email='scopatz@gmail.com',
 	url='http://www.scopatz.com/',
 	packages=['metasci', 'metasci.chem', 'metasci.mathematics', 'metasci.nuke', 'metasci.stats'],
-	package_dir={'metasci': 'src/'}, 
+	package_dir={'metasci': 'src'}, 
 #	package_data={'metasci': []},
 	)
 
