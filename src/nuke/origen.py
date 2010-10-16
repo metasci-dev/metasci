17d02c4fcc106143a1a0865a96b300bb09b2ac26
Anthony Scopatz <scopatz@gmail.com>
2010-10-16 11:52:38 -0500

    Added msno.write_tape5_irradiation() test.

--- src/nuke/origen.py
+++ src/nuke/origen.py
@@ -75,11 +75,11 @@ def out_table_string(out_table_nes, out_table_num):
 
     return s
 
-def make_input_tape5_irradiation(self, ir_type, ir_time, ir_value, nlb, 
-                                 cut_off=10.0**-10, name="TAPE5.INP",
-                                 out_table_nes=(False, False, True),
-                                 out_table_laf=(True,  True,  True),
-                                 out_table_num=None):
+def write_tape5_irradiation(ir_type, ir_time, ir_value, nlb, 
+                            cut_off=10.0**-10, name="TAPE5.INP",
+                            out_table_nes=(False, False, True),
+                            out_table_laf=(True,  True,  True),
+                            out_table_num=None):
     """Writes a TAPE5 files to `name`.
 
     Parameters
--- src/nuke/tests/test_origen.py
+++ src/nuke/tests/test_origen.py
@@ -47,3 +47,32 @@ def test_out_table_string5():
     exp = "8 8 8 8 3 8 8 8 8 3 8 8 8 8 8 8 8 8 8 8 8 8 8 8"
     assert_equal(obs, exp)
 
+def test_write_tape5_irradiation():
+    msno.write_tape5_irradiation("IRP", 100, 0.550, [204, 205, 206], name="test.tape5",
+                                 out_table_nes=(False, False, True), out_table_laf=(True,  False,  True),  out_table_num=[5, 10])
+
+    observed_file = open("test.tape5", 'r')
+    observed = observed_file.read()
+
+    expected = ("  -1\n"
+                "  -1\n"  
+                "  -1\n"
+                "  CUT     5 1.000E-10 -1\n"
+                "  RDA     FIND CROSS SECTION LIBRARY IDENTIFIER NUMBERS IN YOUR LIBRARY FILE\n"
+                "  LIB     0 1 2 3 204 205 206 9 3 0 3 0\n"
+                "  OPTL    8 8 8 8 7 8 8 8 8 7 8 8 8 8 8 8 8 8 8 8 8 8 8 8\n"
+                "  OPTA    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8\n"
+                "  OPTF    8 8 8 8 7 8 8 8 8 7 8 8 8 8 8 8 8 8 8 8 8 8 8 8\n"
+                "  INP     1 -1  0  -1  4  4\n"
+                "  HED     1     IN FUEL\n"
+                "  RDA     ALL IRRADIATION (IRF and IRP) CARDS MUST TAKE PLACE IN BETWEEN BURNUP (BUP) CARDS\n"
+                "  BUP\n"
+                "  IRP     1.0000000000E+02  5.5000000000E-01   1   2   4  2\n"
+                "  BUP\n"
+                "  OUT     2  1 1 0\n"
+                "  END\n")
+
+    assert_equal(observed, expected)
+
+    observed_file.close()
+    os.remove("test.tape5")
